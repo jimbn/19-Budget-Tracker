e472a831bf40d9e51ba2f7bf1f52aa64faf6940c
Jim Nguyen <95522881+jimbn@users.noreply.github.com>
2022-05-18 02:49:26 -0700

    add eventListeners to service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -17,3 +17,45 @@ const FILES_TO_CACHE = [
     './icons/icon-384x384.png',
     './icons/icon-512x512.png'
 ]
+
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(function(cache) {
+            console.log('installing cache: ' + CACHE_NAME);
+            return cache.addAll(FILES_TO_CACHE)
+        })
+    )
+});
+
+self.addEventListener('activavte', function(e) {
+    e.waitUntil(
+        caches.keys().then(function(e) {
+            let cacheKeepList = keyList.filter(function(key){
+                return key.indexOf(APP_PREFIX);
+            });
+            cacheKeepList.push(CACHE_NAME);
+
+            return Promise.all(keyList.map(function(key,i) {
+                if(cacheKeepList.indexOf(key) === -1) {
+                    console.log('deleting cache: ' + keyList[i]);
+                    return caches.delete(keyList[i]);
+                }
+            }));
+        })
+    )
+});
+
+self.addEventListener('fetch', function(e) {
+    console.log('fetch request: ' + e.request.url);
+    e.respondWith(
+        caches.match(e.request).then(function(request) {
+            if(request) {
+                console.log('responding with cache: ' + e.request.url);
+                return request
+            } else {
+                console.log('file is not cached, fetching: ' + e.request.url);
+                return fetch(e.request)
+            }
+        })
+    )
+});
